7539979bf107d996f708ca12e10febff3bee709a
Andres Marquez <andresdmf55@gmail.com>
2022-09-02 15:43:22 -0500

    Adding Section Categories, this section will contain all categories(carousels)

---
+++ src/components/main/sectionCategories/SectionCategories.js
@@ -0,0 +1,14 @@
+import PopularMovies from "./PopularMovies";
+import PopularTvShows from "./PopularTvShows";
+import Trending from "./Trending";
+
+export default function SectionCategories(){
+  return (
+    <div className={`absolute z-50 top-1/5 sm:top-7/20 lg:top-11/12 w-full flex flex-col
+    lg:pl-14 sm:pl-8 pl-4`}>
+      <Trending />
+      <PopularMovies />
+      <PopularTvShows />
+    </div>
+  )
+}
