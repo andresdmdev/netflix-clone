255fb07de0a2b5d733b74d19dfdcadd6564b6776
Andres Marquez <andresdmf55@gmail.com>
2022-09-02 15:22:56 -0500

    Project structure and config App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Home from "./components/main/sectionHome/Home";
+import SectionCategories from "./components/main/sectionCategories/SectionCategories";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-tones-500">
+      <div className="block h-11/12 m-0 p-0">
+        <Header />
+        <Home />
+      </div>
+      <SectionCategories />
     </div>
   );
 }
