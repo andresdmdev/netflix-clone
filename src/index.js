0afd9fec4d1a8f23eccfc53855987213f09e310e
Andres Marquez <andresdmf55@gmail.com>
2022-09-12 12:38:44 -0500

    Fixed bug on index and adding new features in tailwind configs

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import SliderLarge from './components/main/sectionSliderLarge/SliderLarge';
 import { SliderContext } from './context/SliderContext';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <SliderContext>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<App />}>
           <Route path=':id' element={<SliderLarge />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </SliderContext>
 );
