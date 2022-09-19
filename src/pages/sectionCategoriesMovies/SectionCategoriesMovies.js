0a66de6f87a2d8e2495f3601ef7721e4761c24a6
Andres Marquez <andresdmf55@gmail.com>
2022-09-19 16:33:44 -0500

    Fixed bugs and styles of movie categories

--- src/pages/sectionCategoriesMovies/SectionCategoriesMovies.js
+++ src/pages/sectionCategoriesMovies/SectionCategoriesMovies.js
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Header from "../../components/header/Header";
 import ComedyMovies from "../../components/main/sectionCategories/ComedyMovies";
 import FamilyMovies from "../../components/main/sectionCategories/FamilyMovies";
@@ -11,12 +12,13 @@ export default function SectionCategoriesMovies(){
       <Header />
       <Home />
       <SectionCategoriesFilmDetails mediaTypeName='movie' />
-      <div className={`absolute z-50 top-1/5 sm:top-7/20 lg:top-11/12 w-full flex flex-col
+      <div className={`absolute z-50 top-2/6 sm:top-7/20 lg:top-11/12 w-full flex flex-col
         lg:pl-14 sm:pl-8 pl-4 pb-32`}>
         <PopularMovies />
         <ComedyMovies />
         <FamilyMovies />
       </div>
+      <Outlet />
     </>
   )
 }
