5074e144ba12bb7f7e77d2b11d60aab6a97244e0
Andres Marquez <andresdmf55@gmail.com>
2022-09-19 16:25:51 -0500

    Fixed code and bugs of slider large

--- src/components/main/sectionSliderLarge/SliderLarge.js
+++ src/components/main/sectionSliderLarge/SliderLarge.js
@@ -9,6 +9,7 @@ import SliderLargeInfo from './SliderLargeInfo';
 import { SliderLargeTrailerContext } from '../../../context/SliderLargeTrailerContext';
 import { useSelector } from 'react-redux';
 import { keyword } from '../../../services/slices/searchKeywordReducerSlices';
+import navigationSliderLargeHelper from '../../../helpers/navigationSliderLargeHelper';
 
 export default function SliderLarge(){
 
@@ -18,48 +19,21 @@ export default function SliderLarge(){
 
   const keywordValue = useSelector(keyword)
 
-  const storageMediaType = sessionStorage.getItem('mediaType')
-  const storageGenreValue = sessionStorage.getItem('genre')
-
   const { url } = useFilmVideo({ type: extraData.media_type, id: extraData.id, active: true })
 
   function handleCloseBtn(){
 
-    if(storageMediaType === 'movies/genre'){
-
-      navigate(`/movies/genre/${storageGenreValue}`)
-
-    } else if(storageMediaType === 'tv/genre') {
-
-      navigate(`/tv/genre/${storageGenreValue}`)
-
-    } else if(storageMediaType === 'movies'){
-
-      navigate(`/movies`)
-
-    } else if(storageMediaType === 'tv'){
-
-      navigate(`/tv`)
-
-    } else if(storageMediaType === 'search') {
-
-      navigate(`/search/${keywordValue}`)
-
-    } else {
-
-      navigate('/')
-
-    }
+    navigationSliderLargeHelper({ navigate, keywordValue })
   }
 
   return (
     <div className='flex justify-center items-center w-full'>
-      <div onClick={handleCloseBtn} className="w-full h-full overflow-hidden bg-gray-tones-500 bg-opacity-80 brightness-80 fixed inset-0 z-300 top-0 left-0">
+      <div onClick={handleCloseBtn} className="w-full h-full overflow-hidden bg-gray-tones-500 bg-opacity-80 brightness-80 fixed inset-0 z-400 top-0 left-0">
       </div>
-      <div className="relative w-96.5 sm:w-sliderLarge lg:w-sliderLarge sm:h-11/12 lg:h-full flex justify-center z-300 mt-20">
+      <div className="relative w-96.5 sm:w-sliderLarge lg:w-sliderLarge sm:h-11/12 lg:h-full flex justify-center z-500 mt-20">
         <div
           onClick={handleCloseBtn} 
-          className={`absolute -top-3 right-4 sm:-top-8 sm:right-4 hover:ring-2 rounded-full z-300 cursor-pointer 
+          className={`z-500 absolute -top-3 right-4 sm:-top-8 sm:right-4 hover:ring-2 rounded-full cursor-pointer 
           ring-gray-tones-10 w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center bg-gray-tones-500 bg-opacity-60`}>
           <IoMdClose className='w-6 h-6 text-gray-tones-10' />
         </div>
