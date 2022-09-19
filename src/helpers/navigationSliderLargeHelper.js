d6534e4bfafd2ec93e171ed3dc2647a4910b1e44
Andres Marquez <andresdmf55@gmail.com>
2022-09-19 16:21:01 -0500

    Added helper for navigation and routing

---
+++ src/helpers/navigationSliderLargeHelper.js
@@ -0,0 +1,32 @@
+
+export default function navigationSliderLargeHelper({ navigate, keywordValue }){
+
+  const storageMediaType = sessionStorage.getItem('mediaType')
+  const storageGenreValue = sessionStorage.getItem('genre')
+
+  if(storageMediaType === 'movies/genre'){
+
+    return navigate(`/movies/genre/${storageGenreValue}`)
+
+  } else if(storageMediaType === 'tv/genre') {
+
+    return  navigate(`/tv/genre/${storageGenreValue}`)
+
+  } else if(storageMediaType === 'movies'){
+
+    return  navigate(`/movies`)
+
+  } else if(storageMediaType === 'tv'){
+
+    return  navigate(`/tv`)
+
+  } else if(storageMediaType === 'search') {
+
+    return  navigate(`/search/${keywordValue}`)
+
+  } else {
+
+    return  navigate('/')
+
+  }
+}
