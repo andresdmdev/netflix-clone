f7bf751264825285f765e1346012b3054985b354
Andres <andresdmf55@gmail.com>
2022-09-24 18:08:50 -0500

    Added reducer for favorite film list

--- src/services/store/store.js
+++ src/services/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducerSlices from "../slices/categoryReducerSlices";
+import favoriteListReducerSlices from "../slices/favoriteListReducerSlices";
 import searchKeywordReducerSlices from "../slices/searchKeywordReducerSlices";
 
 export const store = configureStore({
   reducer:{
     category: categoryReducerSlices,
-    search: searchKeywordReducerSlices
+    search: searchKeywordReducerSlices,
+    list: favoriteListReducerSlices
   }
 })
