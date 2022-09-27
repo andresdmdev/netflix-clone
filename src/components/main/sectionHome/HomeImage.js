5e2d5c5c9449f89c441600c899fb07727b34ec52
Andres <andresdmf55@gmail.com>
2022-09-27 16:24:38 -0500

    Fixed home section for movil devices

--- src/components/main/sectionHome/HomeImage.js
+++ src/components/main/sectionHome/HomeImage.js
@@ -1,16 +1,16 @@
 
 export default function HomeImage({ imageHome }){
   return (
-    <div className='absolute'>
-      <div className='relative'>
-        <img 
-          fetchpriority="high"
-          src={imageHome} 
-          alt='home'
-          className=" bg-opacity-10"
-        />
-        <div className='bg-gradient-to-t from-gray-tones-500 z-50 w-full sm:h-28 h-16 absolute bottom-0'>
-        </div>
+    <div className='absolute w-full top-0 '>
+      <img 
+        fetchpriority="high"
+        src={imageHome} 
+        alt='home'
+        className="bg-opacity-10"
+        width='100%'
+        height='auto'
+      />
+      <div className='bg-gradient-to-t from-gray-tones-500 z-50 w-full sm:h-28 h-16 absolute bottom-0'>
       </div>
     </div>
   )
