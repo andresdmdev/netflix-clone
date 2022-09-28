38298b3d78363a3a8dc4328217d2622d8781ae20
Andres <andresdmf55@gmail.com>
2022-09-27 19:20:21 -0500

    Fixed spacing of title

--- src/pages/sectionSearchResults/KeywordSearchValue.js
+++ src/pages/sectionSearchResults/KeywordSearchValue.js
@@ -6,8 +6,8 @@ export function KeywordSearchValue(){
   const keywordValue = useSelector(keyword)
 
   return (
-    <div className="flex flex-row sm:gap-4 items-center sm:ml-14 sm:mt-5 sm:mb-10 ml-4 mt-1 mb-6 gap-2">
-      <h3 className="text-base sm:text-xl text-gray-tones-100 font-normal sm:font-semibold">Keyword</h3>
+    <div className="flex flex-row sm:gap-4 items-center sm:ml-14 sm:mt-5 sm:mb-10 ml-4 mt-1 mb-6">
+      <h3 className="text-base sm:text-xl mr-2 text-gray-tones-100 font-normal sm:font-semibold">Keyword</h3>
       <h1 className="m-0 p-0 text-xl sm:text-4xl text-gray-tones-0 font-semibold sm:font-extrabold capitalize">"{keywordValue}"</h1>
     </div>
   )
